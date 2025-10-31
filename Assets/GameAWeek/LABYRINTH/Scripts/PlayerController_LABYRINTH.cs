61924abc4081b088ac6c81909d4d4a6ad20ad2d2
shibata <>
2025-10-31 14:50:15 +0900

    3作品目作成開始(1日目)

--- Assets/GameAWeek/LABYRINTH/Scripts/PlayerController_LABYRINTH.cs
+++ Assets/GameAWeek/LABYRINTH/Scripts/PlayerController_LABYRINTH.cs
@@ -2,22 +2,27 @@ using UnityEngine;
 
 public class PlayerController_LABYRINTH : MonoBehaviour
 {
-    private GameObject field;   //
+    public GameObject field;   //
 
-    private void Start()
-    {
-        
-    }
+    [SerializeField]
+    private float gravityScale = -10.0f;
+    
 
     private void Update()
     {
+        Physics2D.gravity = new Vector2(0,gravityScale);
+
+        
+
         if (Input.GetKey(KeyCode.A))
         {
-            field.transform.rotation = Quaternion.Euler(0, 0, -5);
+            //field.transform.rotation *= Quaternion.Euler(0, 0, -1);
+            field.transform.RotateAround(transform.position, Vector3.forward, -1.0f);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            field.transform.rotation = Quaternion.Euler(0, 0, 5);
+            //field.transform.rotation *= Quaternion.Euler(0, 0, 1);
+            field.transform.RotateAround(transform.position, Vector3.forward, 1.0f);
         }
     }
 }
