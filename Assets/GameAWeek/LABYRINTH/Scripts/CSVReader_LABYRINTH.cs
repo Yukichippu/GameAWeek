1031780fdc8bf5954a58b4ebe6c4d9991fd55351
shibata <>
2025-11-05 19:50:41 +0900

    csvFileでステージを生成させたい(4日目)

---
+++ Assets/GameAWeek/LABYRINTH/Scripts/CSVReader_LABYRINTH.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CSVReader_LABYRINTH : MonoBehaviour
+{
+    [SerializeField]
+    private string CSVFilePath;
+
+    private string[] csvXLine;
+
+    private void Update()
+    {
+        for(int x = 0; x < CSVFilePath.Length; x++)
+        {
+            for (int y = 0; y < CSVFilePath.Length; y++)
+            {
+
+            }
+        }
+    }
+}
--- Assets/GameAWeek/LABYRINTH/Scripts/PlayerController_LABYRINTH.cs
+++ Assets/GameAWeek/LABYRINTH/Scripts/PlayerController_LABYRINTH.cs
@@ -2,27 +2,49 @@ using UnityEngine;
 
 public class PlayerController_LABYRINTH : MonoBehaviour
 {
-    public GameObject field;   //
+    public GameObject field;    //
+
+    private Camera cam;         //
+    private Rigidbody2D rb;     //
 
     [SerializeField]
-    private float gravityScale = -10.0f;
-    
+    private Vector2 gravityScale = new Vector2(0, -10.0f);      //
+    private Vector3 gravityDir;                                 //
+    private Vector3 camForward;                                 //
+    private Vector3 camRight;                                   //
+    private float   cameraDis = -10.0f;                         //カメラの距離
+
+    private void Start()
+    {
+        cam = Camera.main;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        //重力
+        rb.AddForce(gravityDir*gravityScale);
+    }
 
     private void Update()
     {
-        Physics2D.gravity = new Vector2(0,gravityScale);
+        //プレイヤーに追従
+        cam.transform.position = new Vector3(transform.position.x, transform.position.y, cameraDis);
 
-        
+        camForward = cam.transform.forward;
+        camRight = cam.transform.right;
 
+        //現在のカメラの下向きのベクトルを取得
+        gravityDir = -Vector3.Cross(camRight, camForward).normalized;
+
+        //カメラ回転
         if (Input.GetKey(KeyCode.A))
         {
-            //field.transform.rotation *= Quaternion.Euler(0, 0, -1);
-            field.transform.RotateAround(transform.position, Vector3.forward, -1.0f);
+            cam.transform.rotation *= Quaternion.Euler(0, 0, 1);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            //field.transform.rotation *= Quaternion.Euler(0, 0, 1);
-            field.transform.RotateAround(transform.position, Vector3.forward, 1.0f);
+            cam.transform.rotation *= Quaternion.Euler(0, 0, -1);
         }
     }
 }
