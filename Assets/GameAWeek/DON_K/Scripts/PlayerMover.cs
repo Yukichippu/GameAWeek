bc92fa2c0bd26ea276e0b0af26bb911c9816b254
Shibata <>
2025-11-28 14:50:45 +0900

    動けはする

--- Assets/GameAWeek/DON_K/Scripts/PlayerMover.cs
+++ Assets/GameAWeek/DON_K/Scripts/PlayerMover.cs
@@ -1,45 +1,61 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMover : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject[] ladders;
-
     private struct Ladders
     {
         public GameObject ladderObj;
-        public float dis;
+        public Vector2 dis;
     }
     [SerializeField]
     Ladders[] ladder;
 
-    int count = 1;
+    string baseName = "Ladder";
 
     private void Start()
     {
-        for (int i = 0; i < count; i++)
+        //オブジェクトを配列に格納
+        List<Ladders> laddersList = new List<Ladders>();
+        int index = 0;
+        while(true)
         {
-            ladder[i] = new Ladders();
-            ladder[i].ladderObj = GameObject.Find("Ladder" +  i).gameObject;
+            string objName = baseName + index;
+            GameObject obj = GameObject.Find(objName);
 
-            if(ladder[i].ladderObj != null)
-                count++;
+            if (obj == null)
+                break;
+
+            Ladders lad = new Ladders();
+            lad.ladderObj = obj;
+            lad.dis = Vector2.zero;
+
+            laddersList.Add(lad);
+            index++;
         }
+        ladder = laddersList.ToArray();
     }
 
     private void Update()
     {
+        //移動
         float x = Input.GetAxis("Horizontal") * Time.deltaTime;
         transform.position += new Vector3(x, 0, 0);
 
-        //for(int i = 0; i < ladders.Length; i++)
-        //{
-        //    ladder[i].dis = (ladders[i].transform.position-transform.position).magnitude;
-        //    if (ladder[i].dis <= 0.1f)
-        //    {
-        //        float y = Input.GetAxis("Velocity") * Time.deltaTime;
-        //        transform.position += new Vector3(0, y, 0);
-        //    }
-        //}
+        //距離取得
+        for (int i = 0; i < ladder.Length; i++)
+        {
+            if (ladder[i].ladderObj == null) continue;
+
+            Ladders temp = ladder[i];
+            temp.dis = transform.position - ladder[i].ladderObj.transform.position;
+            ladder[i] = temp;
+
+            if (ladder[i].dis.x < 0.1f && ladder[i].dis.y < 0.5f)
+            {
+                float y = Input.GetAxis("Vertical") * Time.deltaTime;
+                transform.position += new Vector3(0, y, 0);
+            }
+        }
     }
 }
